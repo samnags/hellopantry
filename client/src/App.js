cf727e6a846381d0083b337b36601d9cfaa097af
samnags <sam.nagourney@gmail.com>
2018-03-22 14:56:54 -1000

    Front-end setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <a href="http://localhost:5000/auth/google">Log in!</a>
+      </div>
+    );
+  }
+}
+
+export default App;
