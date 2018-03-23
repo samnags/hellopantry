f03248063a8d41412a2b3ce888e2ea6f893ad3b9
samnags <sam.nagourney@gmail.com>
2018-03-22 14:56:45 -1000

    Front-end setup

--- models/User.js
+++ models/User.js
@@ -5,5 +5,5 @@ const userSchema = new Schema({
   googleId: 'string'
 });
 
-// Telling Mongoose to use/createa model called users using the userSchema
+// Telling Mongoose to use/create a model called users using the userSchema
 mongoose.model('users', userSchema);
