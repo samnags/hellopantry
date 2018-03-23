14198b3dab1a25855454385142fa66d068af875c
samnags <sam.nagourney@gmail.com>
2018-03-22 16:10:59 -1000

    Reducer setup

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <a href="/auth/google">Log in!</a>
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// const store = createStore([], {}, applyMiddleware());
+const store = createStore(reducer, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
